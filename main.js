2c54da7c71e5233b4068266907022c8e4625e74b
yogai <yogai@bank.co.jp>
2018-08-25 16:34:13 +0900

    v-bind.prop

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue.js!',
-        list: ['apple', 'banana', 'strawberry']
+        list: ['apple', 'banana', 'strawberry'],
+        scroll: 0
+    },
+    mounted: function() {
+        this.scroll = 100 // 要素のスクロール量を操作
     }
 })
