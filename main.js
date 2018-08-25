47ab40bcf7917ab895c27f354722049006b8e08d
yogai <yogai@bank.co.jp>
2018-08-25 16:41:18 +0900

    クリック関数実装

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ var app = new Vue({
     data: {
         message: 'Hello Vue.js!',
         list: ['apple', 'banana', 'strawberry'],
-        scroll: 0
+        scroll: 0,
+        count: 0
+    },
+    methods: {
+        increment: function() {
+            this.count += 1
+        }
     },
     mounted: function() {
         this.scroll = 100 // 要素のスクロール量を操作
